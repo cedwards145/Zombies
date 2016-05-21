fa6d4699520fbd5004d48cb88de84b2498a47e4e
Cameron Edwards <cameronedwards145@gmail.com>
2016-05-21 20:17:38 +0100

    Included Slick2D and a few maven plugins

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,6 +1,21 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
 public class Program {
     public static void main(String[] args)
     {
-        System.out.println("Hello, world!");
+        try
+        {
+            AppGameContainer appgc;
+            appgc = new AppGameContainer(new Game());
+            appgc.setDisplayMode(640, 480, false);
+            appgc.start();
+        }
+        catch (SlickException ex)
+        {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
